4bfd21f922688a1e45569b0456d71874b15459cd
Andrew <andrew.suponev@gmail.com>
2023-02-18 22:59:58 +0300

    fix display title collection&author name in item card on main page

--- client/src/components/home/home-elements.js/item-card.js
+++ client/src/components/home/home-elements.js/item-card.js
@@ -5,6 +5,16 @@ import ItemTags from '../../item/view-blocks/item-tags';
 
 const ItemCard = ({ _id, title, tags, collectionId, collection }) => {
   let navigate = useNavigate();
+  let collectionTitle = collection.title,
+      authorFirstName = collection.authorId.firstName,
+      authorLastName = collection.authorId.lastName;
+  if (collectionTitle.length > 17) {
+    collectionTitle = `${collectionTitle.slice(0, 17)}...`;
+  };
+  if ((authorFirstName.length + authorLastName.length) > 12) {
+    authorFirstName = `${authorFirstName.slice(0, 8)}...`;
+    authorLastName = `${authorLastName[0]}.`;
+  };
 
   return (
     <Card
@@ -21,9 +31,9 @@ const ItemCard = ({ _id, title, tags, collectionId, collection }) => {
             <Typography fontSize={16} color="#142339" fontWeight={700}>{title}</Typography>
             <ItemTags tags={tags} />
             <Stack direction="row" spacing={1} sx={{ color: "#797E85" }}>
-              <Typography variant="overline">{collection.title}</Typography>
+              <Typography variant="overline">{collectionTitle}</Typography>
               <Typography variant="overline">—</Typography>
-              <Typography variant="overline">by {collection.authorId.firstName} {collection.authorId.lastName}</Typography>
+              <Typography variant="overline">by {authorFirstName} {authorLastName}</Typography>
             </Stack>
           </Stack>
         </CardContent>
