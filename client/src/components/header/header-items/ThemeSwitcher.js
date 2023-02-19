f11c5eaaafe3c58157f61ac398c341bfbd8ff37a
Andrew <andrew.suponev@gmail.com>
2023-02-19 17:13:27 +0300

    change element in header for turn theme

--- client/src/components/header/header-items/ThemeSwitcher.js
+++ client/src/components/header/header-items/ThemeSwitcher.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import { Stack, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { IconButton, Stack, Tooltip } from '@mui/material';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 
 export const ThemeSwitcher = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'ligth');
+
+  const handleChange = () => {
+    if (theme === 'ligth') {
+      setTheme('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+    if (theme === 'dark') {
+      setTheme('ligth');
+      localStorage.setItem('theme', 'ligth');
+    }
+  };
+
   return (
-    <Stack direction="row" spacing={0.8} alignItems="center">
-      <Typography variant="caption" fontSize={14} color="#FFFFFF">Dark Mode</Typography>
-      <div className="form-check form-switch">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          role="button"
-        />
-      </div>
-    </Stack>
+    <Stack sx={{ background: "#192B45", borderRadius: "4px" }}>
+      <Tooltip
+        title={
+          theme === 'dark'
+            ? "Turn on the light"
+            : "Turn off the light"
+        }
+      >
+        <IconButton size="small" onClick={handleChange} sx={{ color: "#FFFFFF" }}>
+          {
+            theme === 'dark'
+              ? <LightModeOutlinedIcon sx={{ height: 22 }} />
+              : <DarkModeOutlinedIcon sx={{ height: 22 }} />
+          }
+        </IconButton>
+      </Tooltip>
+    </Stack >
   );
 }
